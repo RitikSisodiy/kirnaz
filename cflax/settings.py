65c797de6f22a31d58563c3b9581ee82892cc94b
RitikSisodiy <ritik.s10120@gmail.com>
2022-02-01 16:18:50 +0530

    some fix

--- cflax/settings.py
+++ cflax/settings.py
@@ -96,18 +96,18 @@ CKEDITOR_CONFIGS = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'kirnaz',
-    #     'USER': 'userkirnaz', 
-    #     'PASSWORD': 'Flax@2021',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',   #my port is 3306
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kirnaz',
+        'USER': 'userkirnaz', 
+        'PASSWORD': 'Flax@2021',
+        'HOST': '184.168.97.94',
+        'PORT': '3306',   #my port is 3306
+    }
 }
 
 if os.environ.get('enviorment') == "production":
