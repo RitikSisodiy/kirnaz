6dcb97b4b29ea815e1732b0bed0a04e0bb84ff0e
Preeti narware <preetinarware97@gmail.com>
2022-02-05 15:18:53 +0530

    fix last

--- cflax/urls.py
+++ cflax/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('taxfiling/',include('taxfiling.urls')),
     path('dashboard/',include('dashboard.urls'))
     
-] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
