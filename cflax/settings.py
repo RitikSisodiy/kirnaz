6deb098ea56fa8faf3e25ed98d6acc353449ba41
RitikSisodiy <ritik.s10120@gmail.com>
2022-02-17 22:28:39 +0530

    some more fix

--- cflax/settings.py
+++ cflax/settings.py
@@ -127,12 +127,12 @@ if os.environ.get('enviorment') == "production":
         'HOST': 'localhost',
         'PORT': '3306',   #my port is 3306
     }
-    # CHANNEL_LAYERS["default"]= {
-    #         "BACKEND": "channels_redis.core.RedisChannelLayer",
-    #         "CONFIG": {
-    #             "hosts": [("kirnaaz.com", 6379)],
-    #         },
-    #     },
+    CHANNEL_LAYERS["default"]= {
+            "BACKEND": "channels_redis.core.RedisChannelLayer",
+            "CONFIG": {
+                "hosts": [("127.0.0.1", 6379)],
+            },
+        },
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
