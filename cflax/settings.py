20dc1d232b2e5540b7b421fdcaa2d969a6ec5735
RitikSisodiy <ritik.s10120@gmail.com>
2022-02-17 22:46:24 +0530

    some more fix

--- cflax/settings.py
+++ cflax/settings.py
@@ -97,18 +97,18 @@ CKEDITOR_CONFIGS = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    #  'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'kirnaz',
-    #     'USER': 'userkirnaz', 
-    #     'PASSWORD': 'Flax@2021',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306'
-    #  }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kirnaz',
+        'USER': 'userkirnaz', 
+        'PASSWORD': 'Flax@2021',
+        'HOST': 'localhost',
+        'PORT': '3306'
+     }
 }
 
 CHANNEL_LAYERS = {
