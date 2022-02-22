f87c7a23664db8b8c5034c39bf06b350f9e83d83
RitikSisodiy <ritik.s10120@gmail.com>
2022-02-22 18:18:57 +0530

    some fix

--- cflax/settings.py
+++ cflax/settings.py
@@ -102,10 +102,10 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 if os.environ.get('enviorment') != "production":
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.sqlite3',
+        #     'NAME': BASE_DIR / 'db.sqlite3',
+        # }
         #  'default': {
         #     'ENGINE': 'django.db.backends.mysql',
         #     'NAME': 'kirnaz',
